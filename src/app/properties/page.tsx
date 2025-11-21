a46f48a1efe150e3ec3c23ba494c817b15e9e26f
Supermallen1 <masonmstar@gmail.com>
2025-11-20 22:05:48 -0700

    Update page.tsx

--- src/app/properties/page.tsx
+++ src/app/properties/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { supabase } from '../src/lib/supabaseClient';
+import { supabase } from '../../lib/supabaseClient';
 import Link from 'next/link';
 
 export default function PropertiesPage() {
